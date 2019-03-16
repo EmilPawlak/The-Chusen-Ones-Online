c6ac828a2f43018ae61ed5f47471d177b3a52824
Emil Pawlak <Emil.Szymon.Pawlak@gmail.com>
2019-03-16 01:54:15 +0100

    Added basic Grid Layout and added register and login forms

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-function logowanie() {
-  var log = document.getElementById('login');
-  //console.log(log.value)
-  var pass = document.getElementById('password').value;
-  //console.log(pass.value)
+"use strict";
+const WELCOME_SCREEN = document.getElementById("window__welcome");
+const LOGIN_SCREEN = document.getElementById("window__logIn");
+const REGISTER_SCREEN = document.getElementById("window__register");
+
+function changeToLogin() {
+  WELCOME_SCREEN.style.display = "none";
+  LOGIN_SCREEN.style.display = "grid";
 }
 
-function popup(alert){
-  alert(alert);
+function changeToRegister() {
+  WELCOME_SCREEN.style.display = "none";
+  REGISTER_SCREEN.style.display = "grid";
 }
+
+function logIn() {
+  var log = document.getElementById('login')
+  var pass = document.getElementById('password').value;
+}
+
