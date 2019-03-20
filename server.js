dcccab0d0f0ea37c8c568ebef904a91e57a66943
Emil Pawlak <Emil.Szymon.Pawlak@gmail.com>
2019-03-20 15:56:10 +0100

    fixing and updating branch content

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ const server = app.listen(7000, () => {
   console.log(`Express running → PORT ${server.address().port}`);
 }); //nasluchiwanie portu
 
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use(express.static('public'));
+
 MongoClient.connect(url,{ useNewUrlParser: true }, (err, client) => {//połaczenie z bazą
     if (err) {
       console.error(err);
@@ -18,48 +22,62 @@ MongoClient.connect(url,{ useNewUrlParser: true }, (err, client) => {//połaczen
     const db = client.db('test_db');
     const collection = db.collection('user_example');
 
-    app.engine('html', require('ejs').renderFile);
-    app.set('view engine', 'html');
-    app.use(express.static('public'));
+
 
     app.get('/', (req, res) => {
-      res.render('index'); // wyswietlenie htmla indexa
+      res.render('index'); // wyswietlenie htmla
     });
-
-    //app.get('/page2', (req, res) => {
-      //res.render('page2'); // wyswietlenie htmla page2
-    //});
-
 
     app.use(myParser.urlencoded({extended : true}));//<-do ogarniecia
 
     app.post("/", function(req, res) {
 
-          var log = req.body.login;
-          var pass = req.body.password;//przypisanie danych z formularza
+          var log = req.body.login_l;
+          var pass = req.body.password_l;//przypisanie danych z formularza
+          var log_r = req.body.login_r;
+          var pass_r = req.body.password_r;
+          var email_r = req.body.email_r;
+          var button_r=req.body.button_r;
+          var button=req.body.button_l;
 
-          collection.countDocuments({name:log}, function(err, result){
-            if(result){
-                var user = collection.findOne({name:log}, function(err, result){
-                  if (err) {
-                    console.error(err);
-                  }
+          if(button=="zaloguj")
+          {
+            collection.countDocuments({name:log},(err, result) => {
+                if(result){
+                    var user = collection.findOne({name:log}, function(err, result){
+                      if (err) {
+                        console.error(err);
+                      }
 
-                    //console.log(result.name);// wyswietlenie zawartosci bazy
+                        console.log(result.name);// wyswietlenie zawartosci bazy
 
-                    if(result.password===pass)
-                    {
-                      console.log("Udało się zalogować");
-                    } else {
-                        console.log("błedne hasło");
-                    }
-                });
-            } else {
-              console.log("błędny login");
-            }
+                        if(result.password===pass)
+                        {
+                          console.log("Udało się zalogować");
+                        } else {
+                          console.log("Błędne hasło");
+                        }
+                    });
+                } else {
+                  console.log("błędny login");
+                }
           });
-        });
 
+        } else if(button_r=="zarejestruj"){
+
+          collection.countDocuments({name:log_r}, (err,result) => {
+            if(result)
+            {
+              console.log("Taki uzytkownik istnieje");
+            }else
+            {
+              collection.insertOne({name: log_r, password: pass_r, email: email_r});
+            }
+
+          });
+          }
+
+    });
 })
 
 
